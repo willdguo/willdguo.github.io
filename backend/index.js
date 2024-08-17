c38c9e9d0176c83145d1f605338d93c925288313
willdguo <williamguo6@gmail.com>
2024-08-17 16:04:33 -0700

    github pages

--- backend/index.js
+++
@@ -1,9 +0,0 @@
-// const config = require('./utils/config')
-// const server = require('./app')
-// const logger = require('./utils/logger')
-
-// server.listen(config.PORT, () => {
-//     logger.info(`Server running on ${config.PORT}. Happy Ranting!`)
-// })
-
-
