7368ad48811299c84bb657988fc8b346885a2e7c
willdguo <williamguo6@gmail.com>
2024-08-20 18:59:15 -0700

    added research files

--- src/components/ResearchCard.js
+++ src/components/ResearchCard.js
@@ -1,16 +1,17 @@
+import './ResearchCard.css'
 
 function ResearchCard({title, desc, date, links}) {
-    const style = {
-        'borderBottom': '1px solid #ccc',
-        'padding': '10px',
-        'paddingLeft': '0px',
-    }
 
     return (
-        <div style={style}>
+        <div className='researchcard'>
             <h4 style={{fontSize: '20px'}}> {title} </h4>
             <p> {desc} </p>
             <p style={{color: "#888"}}> {date} </p>
+            <p>
+            {links.map(l => (
+                <a href={l.link} target="_blank" rel="noreferrer">{l.text}</a> 
+            ))}
+            </p>
         </div>
     )
 }
--- src/pages/Research.js
+++ src/pages/Research.js
@@ -1,4 +1,6 @@
 import ResearchCard from "../components/ResearchCard";
+// import chemposter from "../files/OregonElectrochemistry_poster-2021_09_18.pdf"
+import chempres from "../files/pnwavs_WG_presentation.pdf"
 
 function Research () {
 
@@ -7,16 +9,22 @@ function Research () {
             'title': "Oja's Algorithm for Streaming PCA",
             'desc': "Tightening/improving existing bounds on the spectral gap for Oja's algorithm to achieve low reconstruction error on arbitrary data matrices.",
             'date': 'Feb 2024 - Present',
+            'links': [],
         },
         {
             'title': "Techno-Economic Comparison of Catalysts for Proton-Exchange Membrane Electrolysis",
             'desc': '',
             'date': 'Feb 2021 - Apr 2022',
+            'links': [                
+                {'link': chempres, 'text': 'Presentation'},
+                // {'link': chemposter, 'text': 'Poster'},
+            ],
         },
         {
             'title': '3D Collision Tracking and Analysis',
             'desc': '',
-            'date': 'Sep 2020 - Mar 2022'
+            'date': 'Sep 2020 - Mar 2022',
+            'links': [],
             // poster: https://aapt-wm.secure-platform.com/a/solicitations/34/sessiongallery/557
             // schedule: https://www.aapt.org/Conferences/WM2022/upload/aapt_wm22_Program-1.pdf
         },
@@ -27,7 +35,13 @@ function Research () {
             <h3 style={{fontWeight:'400'}}> Research </h3>
             <div style={{paddingTop: '20px'}}>
                 {topics.map((topic, idx) => (
-                    <ResearchCard key={idx} title={topic.title} desc={topic.desc} date={topic.date}/>
+                    <ResearchCard 
+                        key={idx} 
+                        title={topic.title} 
+                        desc={topic.desc} 
+                        date={topic.date}
+                        links={topic.links}
+                    />
                 ))}
             </div>
         </div>
